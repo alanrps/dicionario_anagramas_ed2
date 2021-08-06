6fd0fac936f6bfa545baa877d9b6a90b94626a27
alanrps <alanrodrigo636@gmail.com>
2021-08-06 00:31:31 -0300

    # Adicionando roteiro

--- main.c
+++ main.c
@@ -10,27 +10,49 @@ int main (int argc, char **argv) {
     char* palavra;
 
     // Recebe palavra
-    printf("Digite uma palavra: ");
-    scanf("%s", palavra);
+    // printf("Digite uma palavra: ");
+    // scanf("%s", palavra);
 
     // Ordena palavra
-    bubble_sort(palavra);
-    printf("%s", palavra);
+    // bubble_sort(palavra);
+    // printf("%s", palavra);
 
     // Manipula a árvore rubro negra
     ARN_Inserir(&A, 10, 10);
     ARN_Inserir(&A, 1, 1);
     ARN_Inserir(&A, 8, 8);
     ARN_Inserir(&A, 5, 5);
-
     ARN_Imprimir(A, 2, '*');
 
+    // ARN* dado = ARN_Buscar(A, 55);
+
+    // if(!dado) printf("%s", dado);
+
+
     // Cria lista e insere dados
-    ILISTA* lista = LST_Criar();
-    int dado1 = LST_Inserir(lista, 10);
-    int dado2 = LST_Inserir(lista, 20);
-    int dado3 = LST_Inserir(lista, 30);
-    LST_Imprimir(lista);
+    // ILISTA* lista = LST_Criar();
+    // int dado1 = LST_Inserir(lista, 10);
+    // int dado2 = LST_Inserir(lista, 20);
+    // int dado3 = LST_Inserir(lista, 30);
+    // LST_Imprimir(lista);
+
+    // Criar estrutura da palavra
+    // Trocar valor da árvore pela lista
+
+    // Inserção de palavra
+    // 1- Buscar chave ordenada na árvore
+    // - Se não existir "criar" a chave com a palavra ordenada, criar a lista e inserir a palavra 
+    // - Se existir adicionar no array
+
+    // Busca 
+    // 1- Ordenar a palavra recebida
+    // 2 - Buscar uma chave com a palavra
+    // --> Caso encontre
+    // - Ordenar array
+    // - Imprimir cada valor em uma linha
+    // --> Caso não encontre, retornar que não foi encontrado
+
+    // Ler valores do arquivo e adicionar a árvore
 
   return 0;
 }
